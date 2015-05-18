b994d1506f7eafc8d811fb47dc5e7c196afdadda
Alex Zai <azai91@gmail.com>
2015-05-18 12:13:01 -0700

    (feat) fix server serving static

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.resolve(__dirname)));
+app.use(express.static(path.join(__dirname, '/client')));
 app.listen(3000);
